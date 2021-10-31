0261c45fe114dddb5eb427f996a6d2fa0b086567
Placoderm <bkhpali@gmail.com>
2021-10-31 08:03:56 +0530

    add updates

--- app.js
+++ app.js
@@ -1,21 +1,30 @@
 // count variables
 let bambooLeft = 101,
     blackLeft = 64,
-    mahoganyLeft = 0;
+    mahoganyLeft = 0,
+    totalPledged = 89914;
 
 const bambooCountArea = document.querySelector("#bambooCountArea"),
     bambooCountArea2 = document.querySelector("#bambooCountArea2"), //fix area2
     blackCountArea = document.querySelector("#blackCountArea"),
     blackCountArea2 = document.querySelector("#blackCountArea2"),
     mahoganyCountArea = document.querySelector("#mahoganyCountArea"),
-    mahoganyCountArea2 = document.querySelector("#mahoganyCountArea2");
+    mahoganyCountArea2 = document.querySelector("#mahoganyCountArea2"),
+    totalPledgedArea = document.querySelector("#total-pledged");
 
 bambooCountArea.textContent = bambooLeft;
 bambooCountArea2.textContent = bambooLeft;
 blackCountArea.textContent = blackLeft;
 blackCountArea2.textContent = blackLeft;
 mahoganyCountArea.textContent = mahoganyLeft;
-mahoganyCountArea2.textContent = mahoganyLeft;
+(mahoganyCountArea2.textContent = mahoganyLeft),
+(totalPledgedArea.textContent = totalPledged);
+
+// add pledege amount to total------BROKEN
+
+function addPledgeAmountToTotal(amountToAdd) {
+    totalPledged += amountToAdd;
+}
 
 // Back this button
 
@@ -40,4 +49,22 @@ window.onclick = function(event) {
     if (event.target == modalPledgBox) {
         modalPledgBox.style.display = "none";
     }
+};
+// Success
+const successBox = document.querySelector("#success");
+
+// submit pledge
+const submissionAmount = document.querySelector("#submission-amount");
+const pledgeSubmit = document.querySelector("form.modal-pledge-form button");
+pledgeSubmit.addEventListener("click", event => {
+    event.preventDefault();
+    addPledgeAmountToTotal(submissionAmount.value);
+    console.log(totalPledged);
+    modalPledgBox.style.display = "none";
+    successBox.style.display = "block";
+});
+
+const closeSuccessButton = document.querySelector("button.close-success");
+closeSuccessButton.onclick = function() {
+    successBox.style.display = "none";
 };
--- planning.md
+++ planning.md
@@ -4,3 +4,19 @@ click on "select reward" button should
 
 - open modal
 - select the correct card for the select reward button
+
+## pledge submission click
+
+submit the value in the field
+decrease the number remaining of that plege type
+if remaining pledge type ===0 the out-of-stock the pledge card and modal pledge card
+increase the total amount pledged
+increase the bargraph
+close the pledge modal
+open the success modal
+
+## changing modal pledge card selection
+
+deselect current modal pledge card
+select the clicked modal pledge card
+**only one modal pledge card can be selected at a time**
